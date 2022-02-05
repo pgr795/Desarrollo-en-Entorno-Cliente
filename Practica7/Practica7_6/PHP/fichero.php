ed27f7826e4b3a65dcc0455bdb9cc5fb79e3728a
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2022-02-05 15:37:35 +0100

    Delete Practica7 directory

--- Practica7/Practica7_6/PHP/fichero.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $lista=array(
-        array("LG","SONY","SAMSUNG","XIAOMI"),
-        array("ALTAVOCES","MOVIL","TABLETS","TELEVISION")
-        );
-
-    $respuesta= json_encode($lista);
-    echo $respuesta;
-?>
