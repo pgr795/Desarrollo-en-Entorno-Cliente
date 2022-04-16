189999912e077e0cd9c93c983456a8be1715cd7f
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2022-04-16 19:24:09 +0200

    Delete AJAX_FETCH directory

--- AJAX_FETCH/Practica7_2/PHP/fichero.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$nombre=$_REQUEST["nombre"];
-	$apellidos=$_REQUEST["apellidos"];
-    $modulo=$_REQUEST["modulo"];	
-	$notas=rand(4,10);
-	echo $notas;
-?>
--- AJAX_FETCH/Practica7_6/PHP/fichero.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $lista=array("LG","SONY","SAMSUNG","XIAOMI");
-
-    $respuesta= json_encode($lista);
-    echo $respuesta;
-?>
