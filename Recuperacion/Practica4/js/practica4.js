8ae9c1e9358de2ffd7f95f2c6bf1171e46ec3c23
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2022-05-11 10:01:05 +0200

    Update practica4.js

--- Recuperacion/Practica4/js/practica4.js
+++ Recuperacion/Practica4/js/practica4.js
@@ -20,43 +20,19 @@ function aplicarTabla2(){
     let valor=Math.floor(Math.random() * 255)+1;
     let valor2=Math.floor(Math.random() * 255)+1;
     let valor3=Math.floor(Math.random() * 255)+1;
+    let valor4=Math.floor(Math.random() * 255)+1;
+    let valor5=Math.floor(Math.random() * 255)+1;
+    let valor6=Math.floor(Math.random() * 255)+1;
     let filas=$("#cochestabla tr");
     let longitud=filas.length;
     let indice=0;
+    let celdasTH=$(filas).eq(0).children("th");
+    $(celdasTH).odd.css({"background-color":"rgb("+valor+","+valor2+","+valor3+")"});
+    $(celdasTH).even.css({"background-color":"rgb("+valor4+","+valor5+","+valor6+")"});
     while(indice<longitud){
-        let columna=$(filas).eq(indice);
-        let celdasTH=$(columna).children("th");
-        let celdas=$(columna).children("td");
-        $(celdasTH).eq(0).css({"background-color":"rgb("+valor+","+valor2+","+valor3+")"});
-        $(celdas).eq(0).css({"background-color":"rgb("+valor+","+valor2+","+valor3+")"});
-        indice++;
-    }
-
-    valor=Math.floor(Math.random() * 255)+1;
-    valor2=Math.floor(Math.random() * 255)+1;
-    valor3=Math.floor(Math.random() * 255)+1;
-    indice=0;
-
-    while(indice<longitud){
-        let columna=$(filas).eq(indice);
-        let celdasTH=$(columna).children("th");
-        let celdas=$(columna).children("td");
-        $(celdasTH).eq(1).css({"background-color":"rgb("+valor+","+valor2+","+valor3+")"});
-        $(celdas).eq(1).css({"background-color":"rgb("+valor+","+valor2+","+valor3+")"});
-        indice++;
-    }
-
-    valor=Math.floor(Math.random() * 255)+1;
-    valor2=Math.floor(Math.random() * 255)+1;
-    valor3=Math.floor(Math.random() * 255)+1;
-    indice=0;
-
-    while(indice<longitud){
-        let columna=$(filas).eq(indice);
-        let celdasTH=$(columna).children("th");
-        let celdas=$(columna).children("td");
-        $(celdasTH).eq(2).css({"background-color":"rgb("+valor+","+valor2+","+valor3+")"});
-        $(celdas).eq(2).css({"background-color":"rgb("+valor+","+valor2+","+valor3+")"});
+        let celdas=$(filas).eq(indice).children("td");
+        $(celdas).odd.css({"background-color":"rgb("+valor+","+valor2+","+valor3+")"});
+        $(celdas).even.css({"background-color":"rgb("+valor4+","+valor5+","+valor6+")"});
         indice++;
     }
 }
